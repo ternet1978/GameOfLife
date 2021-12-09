3bee727c24ffa77063a54e2e54a0e550401fb354
Administrator <ternet1978@gmail.com>
2021-12-09 13:59:04 +0200

    Secong change

--- src/com/company/Cell.java
+++ src/com/company/Cell.java
@@ -1,4 +1,5 @@
 package com.company;
 
 public class Cell {
+    //65656565416
 }
--- src/com/company/GameField.java
+++ src/com/company/GameField.java
@@ -1,4 +1,7 @@
 package com.company;
 
 public class GameField {
+    public static void main(String[] args) {
+        System.out.println("jijihhhhihi");
+    }
 }
