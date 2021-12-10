44d149812f0d8f7c935343f8dbc68b8fd44d3b19
Administrator <ternet1978@gmail.com>
2021-12-10 16:58:04 +0200

    Almost all)

--- src/com/company/GameField.java
+++ src/com/company/GameField.java
@@ -1,10 +1,11 @@
 package com.company;
 
+import java.util.List;
 import java.util.Random;
 
 public class GameField {
     public static int SCREEN_HEIGHT = 20;
-    public static int SCREEN_WIDTH = 20;
+    public static int SCREEN_WIDTH = 40;
 
     Cell[][] gameScreen = new Cell[SCREEN_HEIGHT][SCREEN_WIDTH];
 
@@ -19,6 +20,70 @@ boolean isCellAlive = r.nextBoolean();
             }
         }
     }
+    void simulateNextStep(){
+        // для каждой ячейки
+
+        // если клетка мертва и соседей 3, то оживить
+        // если клетка жива и соседей 2/3 то жить
+        // иначе убить клетку
+        for (int i = 0; i < SCREEN_HEIGHT; i++) {
+            for (int j = 0; j < SCREEN_WIDTH; j++) {
+                Cell current = gameScreen[i][j];
+                // посчитать кол живых соседей
+                int aliveNeighbours = calculateNeighbours(current,gameScreen);
+                // если клетка мертва и соседей 3, то оживить
+                // если клетка жива и соседей 2/3 то жить
+                // иначе убить клетку
+                if (current.alive==false && aliveNeighbours == 3) {current.alive=true;}
+                else if (current.alive==true && (aliveNeighbours == 3 || aliveNeighbours == 2  ) ) {current.alive=true;}
+                else current.alive =true;
+
+
+            }
+        }
+
+    }
+
+    private int calculateNeighbours(Cell cell, Cell[][] gameScreen) {
+int x = cell.x;
+int y = cell.y;
+
+// a    b   c
+// d  cell  e
+// f    g   h
+
+var a = getCellByCoords(x-1,y-1,gameScreen);
+var b = getCellByCoords(x,y-1,gameScreen);
+var c = getCellByCoords(x+1,y-1,gameScreen);
+
+var d = getCellByCoords(x-1,y,gameScreen);
+var e = getCellByCoords(x+1,y,gameScreen);
+
+var f = getCellByCoords(x-1,y+1,gameScreen);
+var g = getCellByCoords(x,y+1,gameScreen);
+var h = getCellByCoords(x+1,y+1,gameScreen);
+return (int) List.of(a,b,c,d,e,f,g,h).stream()
+        .filter(ccell -> ccell !=null)  .filter(ccell -> ccell.alive)
+        .count();
+
+
+
+    }
+
+    private boolean isAliveCell(int x, int y, Cell[][] gameScreen){
+        if (x<0 || y<0) return false;
+        if (x>SCREEN_WIDTH || y>SCREEN_HEIGHT) return false;
+        return gameScreen[x][y].alive;
+    }
+
+    private Cell getCellByCoords(int x, int y, Cell[][] gameScreen){
+        if (x<0 || y<0) return null;
+        if (x>SCREEN_WIDTH || y>SCREEN_HEIGHT) return false;
+        return gameScreen[x][y].alive;
+    }
+
+
+
 
     void print(){
         for (int i = 0; i < SCREEN_HEIGHT; i++) {
@@ -31,6 +96,9 @@ boolean isCellAlive = r.nextBoolean();
             System.out.println();
             
         }
+        System.out.println();
+        System.out.println("_______________________");
+        System.out.println();
     }
     
     
--- src/com/company/GameOfLife.java
+++ src/com/company/GameOfLife.java
@@ -1,16 +1,23 @@
 package com.company;
 
-import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class GameOfLife {
 
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) throws UnknownHostException, InterruptedException {
 	// write your code here
 //        System.out.println("Hello world!");
 //        String hostAddress = InetAddress.getLocalHost().getHostAddress();
 //        System.out.println(hostAddress);
         GameField g = new GameField();
-        g.print();
+       while (true) {
+           g.simulateNextStep();
+
+           g.print();
+           Thread.sleep(500);
+       }
     }
+
+
+
 }
